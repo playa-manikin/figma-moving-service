f0cdb1f6351b0d1846d6ba014551e58b4a6aa06c
playa-manikin <traurg.doomed@gmail.com>
2023-05-05 10:34:42 +0300

    moved scripts to a file

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function toggleDarkTheme() {
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-theme');
+    const allTags = body.getElementsByTagName('*');
+    for (let i = 0; i < allTags.length; i++) {
+        allTags[i].classList.toggle('dark-theme');
+        }
+    }
