e84f484341aa1ef67298ba21aaeb571cf5efd4f8
playa-manikin <traurg.doomed@gmail.com>
2023-06-16 12:54:25 +0300

    modified:   index.html 	modified:   scripts/script.js 	modified:   styles/index.css 	modified:   styles/index.css.map 	modified:   styles/index.scss

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,34 @@
-const helpButton = document.getElementsByClassName('button-row__help-buton');
+const helpButtons = document.querySelectorAll('.button-row__help-buton');
 const contactDialog = document.getElementById('contact-us-modal');
-const contactDialogClose = document.getElementsByClassName('close-modal');
+const contactDialogClose = document.getElementById('close-modal');
+function showContactUsDialog() {
+    addEventListener('click', () => {
+        contactDialog.showModal()
+    }
+)}
+helpButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        contactDialog.showModal();
+    });
+});
+contactDialogClose.addEventListener('click', () => {
+    contactDialog.close();
+});
+contactDialog.addEventListener("click", e => {
+    const dialogDimensions = contactDialog.getBoundingClientRect()
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+        contactDialog.close()
+    }
+  })
 
-helpButton.onclick = function() {
-    contactDialog.showModal();
-};
 
-
-
-
-
-
+const themeSwitcher = document.querySelector('.theme-switcher')
+themeSwitcher.addEventListener('click', () => {toggleDarkTheme()})
 function toggleDarkTheme() {
     const body = document.querySelector('body');
     body.classList.toggle('dark-theme');
@@ -19,4 +37,12 @@ function toggleDarkTheme() {
         allTags[i].classList.toggle('dark-theme');
         }
     }
+
+
+
+
+
+
+const images = document.querySelectorAll('.services-list__image');
+
 
